5ae6c91745460bae2cece62ea34d3c20466c2cd9
Gabriel Blanco <geblarot96@gmail.com>
2021-08-18 20:00:26 -0500

    Initial commit

---
+++ src/main/java/co/edu/unbosque/C1DemoApplication.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(C1DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/unbosque/c1demo/C1DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.unbosque.c1demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
